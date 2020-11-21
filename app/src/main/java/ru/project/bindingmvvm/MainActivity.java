4680d062232ec900f1fa65672701f868f722790d
Kharin1986 <harin_a_a@mail.ru>
2020-11-21 11:41:41 +0500

    Initial commit

---
+++ app/src/main/java/ru/project/bindingmvvm/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.project.bindingmvvm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
